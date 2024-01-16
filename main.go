0e609d2a5515c23baf923a8e87a5d3de62d36de6
segunkayode1 <segunkayode456@gmail.com>
2024-01-17 00:09:37 +1100

    Add map and mapb commands to Pokedex

--- main.go
+++ main.go
@@ -24,18 +24,34 @@ func getCommandMap() map[string]cliCom {
 			description: "Exits the Pokedex",
 			callback: commandExit,
 		},
+		"map": {
+			name: "map",
+			description: "Lists 20 location areas in the Pokemon World, subsquent uses will list next 20",
+			callback: commandMap,
+		},
+		"mapb": {
+			name: "mapb",
+			description: "Lists 20 location areas in the Pokemon World, subsequent uses will list previous 20",
+			callback: commandMapb,
+		},
 	}
 }
 
+func commandMap() error {
+	return nil
+}
 
+func commandMapb() error {
+	return nil
+}
 func commandHelp() error{
 	fmt.Printf("\nThis is a cli tool for finding Information about pokemon!\n\n")
-	fmt.Printf("Usage\n\n")
+	fmt.Printf("Usage:\n\n")
 	fmt.Printf("\t\t<command> [arguments]\n\n")
 	fmt.Printf("The commands are:\n\n")
 
-	for _, value := range getCommandMap(){
-		fmt.Printf("\t\t%v: %v\n", value.name, value.description)
+	for key, value := range getCommandMap(){
+		fmt.Printf("\t\t%v: %v\n", key, value.description)
 	}
 	return nil
 }
